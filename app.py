46e701e4d2ea6757d737a7f6ff30d194cea04347
punam <punamahire1999@gmail.comm>
2024-05-19 19:01:28 +0530

    Changes for deployment

--- app.py
+++ app.py
@@ -124,5 +124,5 @@ if __name__ == "__main__":
     # app.run(debug=True)  // Comment this while deploymet
     # production command
 
-    serve(app, host="0.0.0.0", port=5000)
+    serve(app, host="0.0.0.0", port=5004)
 
